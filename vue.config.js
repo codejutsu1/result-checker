22bde9e0f7e68f20ee0e8ac3d08ad2a6ee455ebd
codejutsu1 <codejutsu@protonmail.com>
2023-01-20 21:30:41 +0100

    commented some external code

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/result-checker/" : "/",
-};
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === "production" ? "/result-checker/" : "/",
+// };
